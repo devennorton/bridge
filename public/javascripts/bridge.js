3e53066222e1f1ec6fa3f8ad7636007db76c43cd
unknown <dev@Morpheus.(none)>
2011-12-07 20:07:16 -0800

    whoops

--- public/javascripts/bridge.js
+++ public/javascripts/bridge.js
@@ -1,12 +1,15 @@
 var socket = io.connect(location.host);
 console.log('got past connecting');
-socket.on('player', function(player){
-	console.log("got player message");
-	console.log(player);
-	for(i in player.hand){
-		$(#hand).append('<div class="card"><div class="rank">' + 
-			player.hand[i].rank + '</div><div class="suit">' + player.hand[i].suit + 
-			'</div></div>');
-		
-	}
+
+$( function(){
+	socket.on('player', function(player){
+		console.log("got player message");
+		console.log(player);
+		for(i in player.hand){
+			$('#hand').append('<div class="card"><div class="rank">' + 
+				player.hand[i].rank + '</div><div class="suit">' + player.hand[i].suit + 
+				'</div></div>');
+			
+		}
+	});
 });
