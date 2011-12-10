d72a6fec0dcb7202bf8495b122b3df1fd9742eef
unknown <dev@Morpheus.(none)>
2011-12-10 15:42:13 -0800

    somtimes order is important

--- bridge.js
+++ bridge.js
@@ -44,6 +44,53 @@ app.get('/', routes.index);
 
 app.get('/game', routes.game);
 
+
+//all the cards (a convenient constant)
+var CARDS = {
+	SUITS: [ '♣', '♦', '♥', '♠' ],
+	VALUES: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
+}
+
+//Deck Class
+var Deck = function(params) {
+	var _cards = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 
+								13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 
+								26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+								39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51];
+	this.deal = function(n){
+		if (n > _cards.length) return null;
+		var hand = [];
+		for(var i = 0; i < n; i++){
+			hand.push(_cards.splice(Math.floor(Math.random() * _cards.length), 1)[0]);
+		}
+		return hand.sort(function(a, b){ return b - a;});
+	}
+}
+
+//Player Class
+var Player = function(params) {
+	var _position = params.position;
+	var _hand = params.hand;
+	var _played = [];
+	var _bids = [];
+	this.socket = params.socket;
+	
+	Object.defineProperties( this, {
+		position: {
+			get: function(){ return _position }
+		},
+		hand: {
+			get: function(){ return _hand }
+		}
+	});
+	this.play = function(card){
+		var i = _hand.indexOf(card);
+		if (i >= 0){
+			_played.append(_hand.splice(i, 1));
+		}
+	}
+}
+
 var players = {}, next = 1, deck = new Deck();
 
 // socket.io
@@ -85,45 +132,3 @@ io.sockets.on('connection', function (socket) {
 });
 
 
-//all the cards (a convenient constant)
-var CARDS = {
-	SUITS: [ '♣', '♦', '♥', '♠' ],
-	VALUES: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
-}
-
-//Deck Class
-var Deck = function(params) {
-	var _cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51];
-	this.deal = function(n){
-		if (n > _cards.length) return null;
-		var hand = [];
-		for(var i = 0; i < n; i++){
-			hand.push(_cards.splice(Math.floor(Math.random() * _cards.length), 1)[0]);
-		}
-		return hand.sort(function(a, b){ return b - a;});
-	}
-}
-
-//Player Class
-var Player = function(params) {
-	var _position = params.position;
-	var _hand = params.hand;
-	var _played = [];
-	var _bids = [];
-	this.socket = params.socket;
-	
-	Object.defineProperties( this, {
-		position: {
-			get: function(){ return _position }
-		},
-		hand: {
-			get: function(){ return _hand }
-		}
-	});
-	this.play = function(card){
-		var i = _hand.indexOf(card);
-		if (i >= 0){
-			_played.append(_hand.splice(i, 1));
-		}
-	}
-}
