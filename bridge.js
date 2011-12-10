e3b4ba7655bc20b13286b270a71fbca579aa9662
unknown <dev@Morpheus.(none)>
2011-12-10 13:03:54 -0800

    simpler to handle cards and sorted hands.

--- bridge.js
+++ bridge.js
@@ -108,19 +108,14 @@ Player = function(params) {
 
 //Deck Class
 Deck = function(params) {
-	var _cards = [];
-	for (i in CARDS.SUITS){
-		for (j in CARDS.VALUES){
-			_cards.push({suit: CARDS.SUITS[i], val: CARDS.VALUES[j]});
-		}
-	}
+	var _cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51];
 	this.deal = function(n){
 		if (n > _cards.length) return null;
 		var hand = [];
 		for(var i = 0; i < n; i++){
 			hand.push(_cards.splice(Math.floor(Math.random() * _cards.length), 1)[0]);
 		}
-		return hand;
+		return hand.sort.reverse;
 	}
 }
 
--- public/javascripts/bridge.js
+++ public/javascripts/bridge.js
@@ -1,4 +1,9 @@
-﻿var socket = io.connect(location.host);
+﻿var CARDS = {
+	SUITS: [ '♣', '♦', '♥', '♠' ],
+	VALUES: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
+};
+
+var socket = io.connect(location.host);
 console.log('got past connecting');
 
 $( function(){
@@ -7,12 +12,12 @@ $( function(){
 		console.log(player);
 		for(i in player.hand){
 			var redsuit = '';
-			
-			if(player.hand[i].suit == '♦' || player.hand.suit == '♥')
+			var suit =  math.floor(player.hand[i] / 13), rank = player.hand[i] % 13);
+			if(suit == 1 || suit == 2)
 				redsuit = ' redsuit';
 			
-			$('#hand').append('<div class="card' + redsuit + '"><div class="rank">' + 
-				player.hand[i].val + '</div><div class="suit">' + player.hand[i].suit + 
+			$('#hand').append('<div id="'player.hand[i]'" class="card' + redsuit + '"><div class="rank">' + 
+				CARDS.VALUES[rank]  + '</div><div class="suit">' + CARDS.SUITS[suit] + 
 				'</div></div>');
 			
 		}
